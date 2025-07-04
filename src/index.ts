de38843cdcf3817aa804416a6039f0a5f52a9142
nmelgar <nmelgar16013@gmail.com>
2025-07-04 16:28:03 -0600

    Add connection to db

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import swaggerDocs from "./utils/swagger";
 import booksRoutes from "./routes/routes";
+
+// env variables
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 // port of the app
 const PORT = 3000;
+
+// connect to the database
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/books-api";
+
+mongoose.connect(MONGODB_URI);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 // routes
 app.use("/", booksRoutes);
